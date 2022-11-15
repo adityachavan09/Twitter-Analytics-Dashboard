85ef120cab69aa5532b44d4f5ed41036f5ee2c50
aadichavan09 <87874965+aadichavan09@users.noreply.github.com>
2022-11-14 18:28:04 -0800

    added twitter api credentials

---
+++ constants.py
@@ -0,0 +1,5 @@
+# twitter API credentials
+consumer_key = 'HhPnaHmHjgYtnQ5kgtROO751y'
+consumer_secret = 'Gf4PNwrtCQrCtmgKM046WB5wzgAwi5g6wFvOWXvy427GQcnHd6'
+access_token = '964409866902122496-58zNwrlXJ2ZMxE7ufCJto1tnueoxiRj'
+access_secret = 'Ytcl2GJg6p7ZV4GGMMI5gVFSwuQY8zZp7JeKB8OYNdoPd'
